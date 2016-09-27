05bedad1341118d1cad28263bfcb99205b5dd815
Rick Cavallaro <RCavallaro@student.cscc.edu>
2016-09-27 12:29:15 -0400

    First checkin for Project_1

--- Week_04/src/com/Rick_Cavallaro/Week_04/Main.java
+++ Week_04/src/com/Rick_Cavallaro/Week_04/Main.java
@@ -18,31 +18,20 @@ public class Main {
     }
 
     private static String userInputAvgTemps(String currentCity) {
-        String[] strAvgTemps = {"0.0", "0.0", "0.0", "0.0", "0.0"};
-
         System.out.println("Enter average temps for next 5 days separated by spaces for " + currentCity + ":  ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        strAvgTemps = input.split(",", 5);
-
-        double[] dblAvgTemps = {0.0,0.0,0.0,0.0,0.0};
-        int countAvgTemps = strAvgTemps.length;
-        for (int i = 0; i < countAvgTemps; i++ ){
-            dblAvgTemps[i] = Double.parseDouble(strAvgTemps[i]);
-        }
-        double dblTotalAvgTemp = 0.0;
-        for (double dblAvgTemp : dblAvgTemps){
-            dblTotalAvgTemp += dblAvgTemp;
-        }
-        double dblAvgAvgTemp = 0.0;
-        if (countAvgTemps > 0){
-            dblAvgAvgTemp = dblTotalAvgTemp / countAvgTemps;
-        }
         return input;
+    }
+
+    private static void userOutput(String city, double dblAvgAvgTemp){
+        System.out.println("Five day average for " + city + " is " + dblAvgAvgTemp);
     }
 
     public static void main(String[] args) {
         String userCityName;
+        String userAvgTemps;
+        String[] strAvgTemps = {"0.0", "0.0", "0.0", "0.0", "0.0"};
         TreeSet<String> cities = new TreeSet<>();
 
         userCityName = userInputCityName();
@@ -50,8 +39,26 @@ public class Main {
             cities.add(userCityName);
             userCityName = userInputCityName();
         }
+
         for (String city : cities) {
-            userInputAvgTemps(city);
+            userAvgTemps = userInputAvgTemps(city);
+            strAvgTemps = userAvgTemps.split(" ", 5);
+
+            double[] dblAvgTemps = {0.0, 0.0, 0.0, 0.0, 0.0};
+            int countAvgTemps = strAvgTemps.length;
+            for (int i = 0; i < countAvgTemps; i++) {
+                dblAvgTemps[i] = Double.parseDouble(strAvgTemps[i]);
+            }
+            double dblTotalAvgTemp = 0.0;
+            for (double dblAvgTemp : dblAvgTemps) {
+                dblTotalAvgTemp += dblAvgTemp;
+            }
+            double dblAvgAvgTemp = 0.0;
+            if (countAvgTemps > 0) {
+                dblAvgAvgTemp = dblTotalAvgTemp / countAvgTemps;
+            }
+
+            userOutput(city, dblAvgAvgTemp);
         }
     }
 }
