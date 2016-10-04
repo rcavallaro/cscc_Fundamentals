712bb5760185773bee383d2426a29522cce45563
Rick Cavallaro <RCavallaro@student.cscc.edu>
2016-10-04 18:50:57 -0400

    Partial example Week_05

--- Week_05/src/com/Rick_Cavallaro/Week_05/Main.java
+++ Week_05/src/com/Rick_Cavallaro/Week_05/Main.java
@@ -9,71 +9,81 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
-    class WeatherData{
-        // class fields
-        final static String TEMP_UNIT = "F";
-        final static String HUMIDITY_UNIT = "%";
-        final static String PRECIPITATION_UNIT = "%";
-        final static int FREEZING_TEMP = 32;
+class WeatherData {
+    // class fields
+    final static String TEMP_UNIT = "F";
+    final static String HUMIDITY_UNIT = "%";
+    final static String PRECIPITATION_UNIT = "%";
+    final static int FREEZING_TEMP = 32;
 
-        String cityName;
-        double temperature;
-        double humidity;
-        double precipitation;
+    String cityName;
+    double temperature;
+    double humidity;
+    double precipitation;
 
-        WeatherData(String cityName, double temperature, double humidity,
-                    double precipitation){
-            this.cityName = cityName;
-            this.temperature = temperature;
-            this.humidity = humidity;
-            this.precipitation = precipitation;
-        }
+    WeatherData(String cityName, double temperature, double humidity,
+                double precipitation) {
+        this.cityName = cityName;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.precipitation = precipitation;
+    }
 
-        private static double celciusToFahrenheit(double celsiusTemperature) {
-            double fahrenheitTemperature = 9.0 / 5 * celsiusTemperature + 32;
-            return fahrenheitTemperature;
-        }
 
-        private static double fahrenheitToCelciusTo(double fahrenheitTemperature) {
-            double celsiusTemperature = (fahrenheitTemperature - 32.0) * 5.0 / 9;
-            return celsiusTemperature;
+    private static double celciusToFahrenheit(double celsiusTemperature) {
+        double fahrenheitTemperature = 9.0 / 5 * celsiusTemperature + 32;
+        return fahrenheitTemperature;
+    }
 
-        }
-    public class Main {
-        static List<String> combine(List<String> firstList, List<String> secondList) {
-            List<String> listNew = new ArrayList<String>();
-            for (String thing: firstList){
-                listNew.add(thing);
+    private static double fahrenheitToCelciusTo(double fahrenheitTemperature) {
+        double celsiusTemperature = (fahrenheitTemperature - 32.0) * 5.0 / 9;
+        return celsiusTemperature;
+
+    }
+}
+
+public class Main {
+/*
+            static List<String> combine(List<String> firstList, List<String> secondList) {
+                List<String> listNew = new ArrayList<String>();
+                for (String thing : firstList) {
+                    listNew.add(thing);
+                }
+                for (String thing : secondList) {
+                    listNew.add(thing);
+                }
+                return listNew;
             }
-            for (String thing: secondList){
-                listNew.add(thing);
-            }
-            return listNew;
-        }
-        static Map<String, Integer> assignTemps(String[] cities, int[] temps) {
-            Map<String, Integer> temperatures = new HashMap<>();
+*/
 
-            for (int i = 0; i < cities.length; i++) {
-                temperatures.put(cities[i], temps[i]);
-            }
-        return temperatures;
-        }
+/*
+            static Map<String, Integer> assignTemps(String[] cities, int[] temps) {
+                Map<String, Integer> temperatures = new HashMap<>();
 
-        static void displayFreezing(Map<String, Integer> temps){
-            for (String city: temps.keySet()) {
-                if (temps.get(city) <= 32) {
-                    System.out.println("It's cold in " + city + "!");
+                for (int i = 0; i < cities.length; i++) {
+                    temperatures.put(cities[i], temps[i]);
+                }
+                return temperatures;
+            }
+*/
+
+/*
+            static void displayFreezing(Map<String, Integer> temps) {
+                for (String city : temps.keySet()) {
+                    if (temps.get(city) <= 32) {
+                        System.out.println("It's cold in " + city + "!");
+                    }
                 }
             }
-        }
+*/
 
-        public static void main(String[] args) {
-            WeatherData columbus = new WeatherData("Columbus", 70,65,10);
+    public static void main(String[] args) {
+        WeatherData columbus = new WeatherData("Columbus", 70, 65, 10);
 
-            System.out.println("City name: " + columbus.cityName);
-            System.out.println("Temperature: " + columbus.temperature + WeatherData.TEMP_UNIT);
-            System.out.println("Humidity: " + columbus.humidity + WeatherData.HUMIDITY_UNIT);
-            System.out.println("Chance of precipition: " + columbus.precipitation + WeatherData.PRECIPITATION_UNIT);
+        System.out.println("City name: " + columbus.cityName);
+        System.out.println("Temperature: " + columbus.temperature + WeatherData.TEMP_UNIT);
+        System.out.println("Humidity: " + columbus.humidity + WeatherData.HUMIDITY_UNIT);
+        System.out.println("Chance of precipition: " + columbus.precipitation + WeatherData.PRECIPITATION_UNIT);
           /*  String[] cities = {"Cleveland", "New York", "Dallas", "Tampa"};
             int[] temps = {15, 30, 45, 70};
             Map tempMap = assignTemps(cities, temps);
@@ -92,6 +102,6 @@ import java.util.List;
             for (String thing: listC){
                 System.out.println(thing);
             }*/
-        }
     }
+}
 
