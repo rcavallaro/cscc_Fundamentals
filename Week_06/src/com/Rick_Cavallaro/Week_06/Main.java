eac741e652fcec4301f6ec3912d91457d12f2438
Rick Cavallaro <RCavallaro@student.cscc.edu>
2016-10-04 20:28:13 -0400

    Week_06 Checking for exceptions

--- Week_06/src/com/Rick_Cavallaro/Week_06/Main.java
+++ Week_06/src/com/Rick_Cavallaro/Week_06/Main.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
-    private static int promptInt() {
+    private static int promptInt() throws Exception{
         System.out.println("Enter an integer:  ");
         String input = scanner.nextLine();
         int intInput = Integer.parseInt(input);
         return intInput;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         int[] values = {1,2,3};
         int which = promptInt();
         System.out.println((values[which]));
