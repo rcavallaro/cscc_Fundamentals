657b25e4ee9819fa8e150247a83ea2102f608b38
Rick Cavallaro <RCavallaro@student.cscc.edu>
2016-10-11 19:14:41 -0400

    Week_06 with error handling

--- Week_06/src/com/Rick_Cavallaro/Week_06/Main.java
+++ Week_06/src/com/Rick_Cavallaro/Week_06/Main.java
@@ -8,16 +8,36 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
-    private static int promptInt() throws Exception{
-        System.out.println("Enter an integer:  ");
+    private static int promptInt(String prompt) throws Exception{
+        System.out.println(prompt + ":  ");
         String input = scanner.nextLine();
-        int intInput = Integer.parseInt(input);
+        try{
+            int intInput = Integer.parseInt(input);
+        }
+        catch (NumberFormatException e){
+            System.out.println(input + " is not a valid integer.");
+            input = scanner.nextLine();
+        }
         return intInput;
     }
 
+    private static int promptDouble(String prompt) throws Exception{
+        System.out.println(prompt + ":  ");
+        String input = scanner.nextLine();
+        try{
+            double dblInput = Double.parseDouble(input);
+        }
+        catch (NumberFormatException e){
+            System.out.println(input + " is not a valid double.");
+            input = scanner.nextLine();
+        }
+        return dblInput;
+    }
+
+
     public static void main(String[] args) throws Exception{
         int[] values = {1,2,3};
-        int which = promptInt();
+        int which = promptInt("Enter an integer");
         System.out.println((values[which]));
     }
 }
