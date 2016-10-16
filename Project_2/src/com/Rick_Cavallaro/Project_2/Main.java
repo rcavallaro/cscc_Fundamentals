f6bed24609ec75b1af6b5700e197faf28842e3e6
Rick Cavallaro <RCavallaro@student.cscc.edu>
2016-10-16 08:54:25 -0400

    Project_2 initial check in after copy of Project_1

---
+++ Project_2/src/com/Rick_Cavallaro/Project_2/Main.java
@@ -0,0 +1,85 @@
+package com.Rick_Cavallaro.Project_2;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+/**
+ * Created by Rick on 10/16/2016.
+ */
+public class Main {
+    private static String userInputMenuOption() {
+        System.out.println("Please choose an option:\n" +
+                "(1) Add a task.\n" +
+                "(2) Remove a task.\n" +
+                "(3) Update a task.\n" +
+                "(4) List all tasks.\n" +
+                "(5) List all tasks of a certain priority.\n" +
+                "(0) Exit.\n\n");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static String userInputTaskDescription() {
+        System.out.println("Please enter a task description: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private static String userInputTaskNumber() {
+        System.out.println("Please enter a task number: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public static void main(String[] args) {
+        String userMenuOption;
+        String userTaskDescription;
+        String userTaskNumber;
+        String[] strTask = new String[99];
+        int intTaskCount = 0;
+        int intTaskNumber = 0;
+
+        userMenuOption = userInputMenuOption();
+        while (!userMenuOption.equals("0")) {
+            switch (userMenuOption) {
+                case "1":
+                    // Add a task to the end of the list
+                    userTaskDescription = userInputTaskDescription();
+                    strTask[intTaskCount] = userTaskDescription;
+                    intTaskCount += 1;
+                    break;
+                case "2":
+                    // Remove the specified task
+                    userTaskNumber = userInputTaskNumber();
+                    intTaskNumber = Integer.parseInt(userTaskNumber);
+                    while (intTaskNumber >= 0 && intTaskNumber < intTaskCount){
+                        System.out.println("Shifting item " + (intTaskNumber + 1));
+                        strTask[intTaskNumber] = strTask[intTaskNumber + 1];
+                        intTaskNumber += 1;
+                    }
+                    intTaskCount -= 1;
+                    break;
+                case "3":
+                    // Update the specified task
+                    userTaskNumber = userInputTaskNumber();
+                    intTaskNumber = Integer.parseInt(userTaskNumber);
+                    if (intTaskNumber >= 0 && intTaskNumber < intTaskCount){
+                        userTaskDescription = userInputTaskDescription();
+                        strTask[intTaskNumber] = userTaskDescription;
+                        System.out.println("Task (" + userTaskNumber + ") Updated.");
+                    }
+                    break;
+                case "4":
+                    // List all known tasks
+                    System.out.println("\nTasks\n");
+                    for (int i = 0; i < intTaskCount; i++){
+                        System.out.println("(" + i + ") " + strTask[i]);
+                    }
+                    System.out.println("\n");
+                    break;
+            }
+            userMenuOption = userInputMenuOption();
+        }
+    }
+}
