da40684820f7167447a67f931d032d717c66eddd
Rick Cavallaro <RCavallaro@student.cscc.edu>
2016-10-22 20:35:17 -0400

    Project_2 working with Task class handling missing task number

--- Project_2/src/com/Rick_Cavallaro/Project_2/Main.java
+++ Project_2/src/com/Rick_Cavallaro/Project_2/Main.java
@@ -27,6 +27,8 @@ class Task{
     }
 }
 
+/*
+// This was an idea for a future project not fully developed
 class Tasks{
     //TreeSet<Task> tasks = new TreeSet<>();
     Map<String, Task> tasks = new TreeMap<>();
@@ -44,6 +46,7 @@ class Tasks{
         return tasks.get(taskNumber);
     }
 }
+*/
 
 public class Main {
     private static String userInputMenuOption() {
@@ -64,13 +67,27 @@ public class Main {
         return scanner.nextLine();
     }
 
+    private static String userInputPriority(String strPrompt) {
+        String strValidPriorities = "012345";
+        System.out.println(strPrompt);
+        Scanner scanner = new Scanner(System.in);
+        String priority = "";
+        priority = scanner.nextLine();
+        while (priority.length()==0 || (priority.length()==1 && !strValidPriorities.contains(priority))) {
+            System.out.println("Priority must be 0 to 5");
+            System.out.println(strPrompt);
+            priority = scanner.nextLine();
+        } ;
+        return priority;
+    }
+
     public static void main(String[] args) {
         String userMenuOption;
         String strPrompt =  "";
         String userTaskDescription;
         String userTaskNumber;
         Map<String, Task> tasks2 = new TreeMap<>();
-        Tasks tasks = new Tasks();
+        //Tasks tasks = new Tasks();
         Integer intTaskCount = 0;
         int intTaskNumber = 0;
 
@@ -83,8 +100,8 @@ public class Main {
                     Task task = new Task("","","","");
                     task.name = userInput("Enter the new task's name: ");
                     task.description = userInput("Enter the new task's description: ");
-                    task.priority = userInput("Enter the new task's priority: ");
-                    tasks.add(task);
+                    task.priority = userInputPriority("Enter the new task's priority: ");
+                    //tasks.add(task);
                     task.number = intTaskCount.toString();
                     tasks2.put(task.number, task);
                     intTaskCount += 1;
@@ -104,7 +121,7 @@ public class Main {
                     else{
                         task.name = userInput("Enter the task's new name: ");
                         task.description = userInput("Enter the task's new description: ");
-                        task.priority = userInput("Enter the new task's new priority: ");
+                        task.priority = userInputPriority("Enter the new task's new priority: ");
                         System.out.println("Task (" + userTaskNumber + ") Updated.");
                     }
                     break;
@@ -125,7 +142,8 @@ public class Main {
                     break;
                 case "5":
                     // List all known tasks
-                    String strListTaskPriority = userInput("Enter priority of tasks to list: ");
+                    String strListTaskPriority = userInputPriority
+                            ("Enter priority of tasks to list: ");
                     System.out.println("\nTasks\n");
                     for(Map.Entry<String,Task> entry : tasks2.entrySet()) {
                         String key = entry.getKey();
---
+++ Week_08/src/com/Rick_Cavallaro/Week_08/Week_08.java
@@ -0,0 +1,10 @@
+package com.Rick_Cavallaro.Week_08;
+
+/*
+ * Created by Rick on 10/18/2016.
+ */
+public class Week_08 {
+    public static void main(String[] args) {
+
+    }
+}
